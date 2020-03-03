e243d1433598c7145e029f3a0b3334f33ab33407
henriqueconte <henriqueconte6@gmail.com>
2020-03-03 17:33:12 -0300

    first commit

---
+++ ArduinoClockIn/ArduinoClockIn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArduinoClockIn
+//
+//  Created by Henrique Figueiredo Conte on 03/03/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
