ffc69d32b5997c4f5d31a96246c939e322e10b29
Pedro Lopes <pedroesl96@gmail.com>
2020-03-10 12:54:42 -0300

    Implemented CRUD airtable

---
+++ ArduinoClockIn/ArduinoClockIn/AirtableDB/AirtableClockinDB.swift
@@ -0,0 +1,47 @@
+//
+//  AirtableClockinDB.swift
+//  ArduinoClockIn
+//
+//  Created by Pedro Lopes on 09/03/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+import Foundation
+import SwiftAirtable
+
+class AirtableClockinDB {
+    let apiKey = "keyHOVouGJpK0Dde2"
+    let apiBaseUrl = "https://api.airtable.com/v0/app0ZyNNHPCyRjdzZ"
+    let airtable: Airtable!
+    let userId: String!
+    let tableClockins = "ClockIns"
+    
+    init(schema: AirtableTableSchema, userId: String) {
+        airtable = Airtable(apiKey: apiKey, apiBaseUrl: apiBaseUrl, schema: schema)
+        self.userId = userId
+    }
+    
+    func getMyClockins(includeClockOuts: Bool, handler: @escaping ([Clockin], Error?) -> Void) {
+        airtable.fetchAll(table: tableClockins) { (objects: [Clockin], error: Error?) in
+            if let error = error {
+                handler([], error)
+            } else {
+                let clockins = objects.filter{
+                    $0.owner == self.userId && (includeClockOuts ? true : !$0.isClockOut)
+                }
+                handler(clockins, nil)
+            }
+        }
+    }
+    
+    func createClockin(clockin: Clockin, handler: @escaping (Bool, Error?) -> Void) {
+        airtable.createObject(with: clockin, inTable: tableClockins) { object, error in
+            
+            if let error = error {
+                handler(false, error)
+            } else {
+                handler(true, nil)
+            }
+        }
+    }
+}
---
+++ ArduinoClockIn/ArduinoClockIn/AirtableDB/AirtableUserDB.swift
@@ -0,0 +1,9 @@
+//
+//  AirtableUserDB.swift
+//  ArduinoClockIn
+//
+//  Created by Pedro Lopes on 09/03/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+import Foundation
